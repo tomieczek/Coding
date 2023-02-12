651647c44aff479cfb9e2f326f293c349c89b06c
Tomas Vavra <tomasekv2@gmail.com>
2023-02-12 22:00:42 +0100

    Update #5

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log("Hello world!"+name);
+
+function toggleLanguage(id){ // Changes language to the other one 
+const language = document.getElementById(id)
+if (language == "cz"){
+language.
+}
+}
+function togglePassword(id){ // Changes type input from password to text
+    const element = document.getElementById(id);
+    if (!element){
+        return
+    }
+    if (element.type == "password") {
+        element.type = "text"
+    } else {
+        element.type = "password"
+    }
+}
