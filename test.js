a6753f217ac3743b08ee31ee8cbd2512900fa1c0
Tomas Vavra <tomasekv2@gmail.com>
2023-02-14 22:54:30 +0100

    Fixing syntax + event listener doesnt work

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
+const username = document.getElementById('name')
+const email = document.getElementById("email")
+const password1 = document.getElementById("password1")
+const password2 = document.getElementById("password2")
+const errorElement = document.getElementById('error')
+const form = document.querySelector('form')
+
+username.value = "owo"
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (username.value === '' || username.value == null){
+        // messages.push 
+       console.log('Name is required')
+       console.log(e)
+    }
+
+ })
 
 function toggleLanguage(id){ // Changes language to the other one 
 const language = document.getElementById(id)
 if (language == "cz"){
-language.
+language
 }
 }
 function togglePassword(id){ // Changes type input from password to text
@@ -16,3 +33,4 @@ function togglePassword(id){ // Changes type input from password to text
         element.type = "password"
     }
 }
+
