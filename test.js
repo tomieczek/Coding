4076084a680fdfe4d29aba39a0306ee412b036a3
Tomas Vavra <tomasekv2@gmail.com>
2023-02-21 20:33:36 +0100

    Update #8

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-// Please stay like this
 const form = document.getElementById("form");
 const username = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const checkbox = document.getElementById("checkbox");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   validateInputs();
+
 });
 
 const isValidEmail = (email) => {
@@ -24,25 +25,31 @@ const isValidPassword = (password) => {
 
 const setError = (element, message) => {
   const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
+  const errorDisplay = inputControl.querySelector(".errorMessage");
 
-  errorDisplay.innerText = message;
+  if(errorDisplay){
+    errorDisplay.innerText = message;
+  }
   inputControl.classList.add("error");
+  
 };
 const setSuccess = (element, message) => {
   const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
+  const errorDisplay = inputControl.querySelector(".errorMessage");
 
-  errorDisplay.innerText = "";
+  if (errorDisplay) {
+    errorDisplay.innerText = "";
+  }
   inputControl.classList.remove("error");
 };
+
+
 
 const validateInputs = () => {
   const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const password2Value = password2.value.trim();
-  console.log(usernameValue);
   if (usernameValue == "" || usernameValue == null) {
     setError(username, "Username is required");
   } else {
@@ -69,8 +76,21 @@ const validateInputs = () => {
     setError(password2, "Password is required");
   } else if (passwordValue !== password2Value) {
     setError(password2, "Doesn't match with Password");
-  } else {setSuccess(password2, "")}
+  } else {
+    setSuccess(password2, "");
+  }
+  if (!checkbox.checked) {
+    setError(checkbox, "Please select this checkbox to proceed.");
+  } else {
+    setSuccess(checkbox, "");
+  }
+
+  const errors = form.querySelectorAll(".error");
+  if (errors.length === 0) {
+    form.submit(); 
+  }
 };
+
 
 function toggleLanguage(id) {
   // Changes language to the other one
